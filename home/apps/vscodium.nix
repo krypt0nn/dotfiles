f779138bcd0a2a53786d50dd9ae524decf2e9d82
Nikita Podvirnyi <krypt0nn@vk.com>
2024-05-24 11:26:25 +0200

    feat(home): updated `vscodium` package

--- home/apps/vscodium.nix
+++ home/apps/vscodium.nix
@@ -1,5 +1,31 @@
 { pkgs, ... }: {
-    home.packages = with pkgs; [
-        vscodium
-    ];
+    programs.vscode = {
+        enable = true;
+
+        # package = pkgs.vscodium.fhsWithPackages (pkgs: with pkgs; [
+        #     cargo
+        #     clippy
+        # ]);
+
+        package = pkgs.vscodium.fhs;
+
+        extensions = with pkgs.vscode-extensions; [
+            # Misc
+            # aaron-bond.better-comments
+            # vivaxy.vscode-conventional-commits
+            mhutchie.git-graph
+            gruntfuggly.todo-tree
+
+            # Debugger
+            vadimcn.vscode-lldb
+
+            # Nix
+            bbenoist.nix
+
+            # Rust
+            bungcip.better-toml
+            serayuzgur.crates
+            rust-lang.rust-analyzer
+        ];
+    };
 }
