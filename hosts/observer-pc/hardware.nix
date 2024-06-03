bdf430e2b0c43987784b433e90eee28afaeb862c
Nikita Podvirnyi <krypt0nn@vk.com>
2024-06-03 13:44:30 +0200

    fix: fixed bcachefs multi-device loading warnings

--- hosts/observer-pc/hardware.nix
+++ hosts/observer-pc/hardware.nix
@@ -8,27 +8,34 @@
     boot.kernelModules = [ "kvm-amd" ];
     boot.extraModulePackages = [ ];
 
-    fileSystems."/" = {
-        device = "UUID=daddd060-bcc7-4c43-92b7-4fe386b9e875";
-        fsType = "bcachefs";
-        options = [
-            "noatime"
-            "nodiratime"
-            "discard"
-            "ssd"
-            "compression=lz4"
-            "background_compression=zstd"
-        ];
-    };
+    fileSystems = {
+        "/" = {
+            device = "/dev/sda:/dev/sdb:/dev/nvme0n1p3"; # UUID=daddd060-bcc7-4c43-92b7-4fe386b9e875
+            fsType = "bcachefs";
+            options = [
+                "noatime"
+                "nodiratime"
+                "discard"
+                "ssd"
+                "compression=lz4"
+                "background_compression=zstd"
+            ];
+        };
 
-    fileSystems."/boot" = {
-        device = "/dev/disk/by-uuid/A07C-1DC5";
-        fsType = "vfat";
-        options = [ "fmask=0022" "dmask=0022" ];
+        "/boot" = {
+            device = "/dev/nvme0n1p1"; # /dev/disk/by-uuid/A07C-1DC5
+            fsType = "vfat";
+            options = [
+                "fmask=0022"
+                "dmask=0022"
+            ];
+        };
     };
 
     swapDevices = [
-        { device = "/dev/disk/by-uuid/25bc019c-d826-4499-92d8-d5d97cefc2b3"; }
+        {
+            device = "/dev/nvme0n1p2"; # /dev/disk/by-uuid/25bc019c-d826-4499-92d8-d5d97cefc2b3
+        }
     ];
 
     networking.useDHCP = lib.mkDefault true;
