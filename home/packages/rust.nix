fec4e143c4ff2d406af81c17fb8e34120702688e
Nikita Podvirnyi <krypt0nn@vk.com>
2024-06-09 14:47:36 +0200

    feat: persist rust folders

--- home/packages/rust.nix
+++ home/packages/rust.nix
@@ -1,11 +1,23 @@
-{ pkgs-unstable, ... }: {
+{ flakeConfig, pkgs-unstable, ... }: {
     home.packages = with pkgs-unstable; [
         cargo
         clippy
+        rustc
+        rustfmt
+        rustup
     ];
 
     home.sessionVariables = {
         RUST_SRC_PATH = "${pkgs-unstable.rust.packages.stable.rustPlatform.rustLibSrc}";
         RUST_BACKTRACE = 1;
+    };
+
+    home.persistence."/persistent/home/${flakeConfig.username}" = {
+        allowOther = false;
+
+        directories = [
+            ".rustup"
+            ".cargo"
+        ];
     };
 }
