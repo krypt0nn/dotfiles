800a21fbd73a1e282539efa4ae400210d43f8166
Nikita Podvirnyi <krypt0nn@vk.com>
2024-07-04 10:37:13 +0200

    feat: updated network settings

--- nixos/network.nix
+++ nixos/network.nix
@@ -4,9 +4,21 @@
         "0.0.0.0" = [
             # Wuthering Waves
             "pc.crashsight.wetest.net"
+
+            # Hoyoverse games
+            "log-upload-os.hoyoverse.com"
+            "overseauspider.yuanshen.com"
         ];
     };
 
     # Configure networking
     networking.networkmanager.enable = true;
+
+    # DNS nameservers
+    networking.nameservers = [
+        "1.1.1.1"
+        "1.0.0.1"
+        "2606:4700:4700::1111"
+        "2606:4700:4700::1001"
+    ];
 }
