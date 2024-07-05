c51dae3753a022e312f6d79a7903b6d05b67f40e
Nikita Podvirnyi <krypt0nn@vk.com>
2024-07-05 23:01:28 +0200

    feat: updated networking config

--- nixos/network.nix
+++ nixos/network.nix
@@ -1,31 +1,37 @@
 { flakeConfig, ... }: {
-    networking.hosts = {
-        # Block telemetry servers
-        "0.0.0.0" = [
-            # Wuthering Waves
-            "pc.crashsight.wetest.net"
+    networking = {
+        # Configure networking
+        networkmanager = {
+            enable = true;
+            dns = "none";
+        };
 
-            # Hoyoverse games
-            "log-upload-os.hoyoverse.com"
-            "overseauspider.yuanshen.com"
+        # Setup hosts file
+        hosts = {
+            # Block telemetry servers
+            "0.0.0.0" = [
+                # Wuthering Waves
+                "pc.crashsight.wetest.net"
+
+                # Hoyoverse games
+                "log-upload-os.hoyoverse.com"
+                "overseauspider.yuanshen.com"
+            ];
+        };
+
+        # DNS nameservers
+        nameservers = [
+            # Cloudflare DNS
+            # "1.1.1.1"
+            # "1.0.0.1"
+            # "2606:4700:4700::1111"
+            # "2606:4700:4700::1001"
+
+            # Yandex DNS
+            "77.88.8.88"
+            "77.88.8.2"
+            "2a02:6b8::feed:bad"
+            "2a02:6b8:0:1::feed:bad"
         ];
     };
-
-    # Configure networking
-    networking.networkmanager.enable = true;
-
-    # DNS nameservers
-    networking.nameservers = [
-        # Cloudflare DNS
-        # "1.1.1.1"
-        # "1.0.0.1"
-        # "2606:4700:4700::1111"
-        # "2606:4700:4700::1001"
-
-        # Yandex DNS
-        "77.88.8.88"
-        "77.88.8.2"
-        "2a02:6b8::feed:bad"
-        "2a02:6b8:0:1::feed:bad"
-    ];
 }
