4d7752d79cc936c861ffc1872a70d8a88ef87c8d
Nikita Podvirnyi <krypt0nn@vk.com>
2024-07-06 18:40:43 +0200

    feat: updated syncthing service

--- users/observer/syncthing.nix
+++ users/observer/syncthing.nix
@@ -1,32 +1,38 @@
 { ... }: {
-    services = {
-        syncthing = {
-            enable = true;
+    services.syncthing = {
+        enable = true;
 
-            user = "observer";
+        user = "observer";
 
-            dataDir = "/home/observer/.syncthing";
-            configDir = "/home/observer/.syncthing/.config";
+        dataDir = "/home/observer/.syncthing";
+        configDir = "/home/observer/.syncthing/.config";
 
-            overrideDevices = true;
-            overrideFolders = true;
+        overrideDevices = true;
+        overrideFolders = true;
 
-            settings = {
-                devices = {
-                    "observer-pc" = { id = "4TX2PRI-LGLEIOD-UFIWU7X-W3MVXTM-QAFSX2M-IJXUYZI-4NOSWPC-7GXT4AB"; };
-                    # "observer-laptop" = { id = "DEVICE-ID-GOES-HERE"; };
+        settings = {
+            devices = {
+                "observer-pc"     = { id = "4TX2PRI-LGLEIOD-UFIWU7X-W3MVXTM-QAFSX2M-IJXUYZI-4NOSWPC-7GXT4AB"; };
+                "observer-laptop" = { id = "JWIJBIA-6ZQUP33-GB4OEC3-KQPW3CM-YFAZDDW-LQQNPWE-DI6WYXI-XVSVKQR"; };
+            };
+
+            folders = {
+                documents = {
+                    path = "/home/observer/Documents";
+
+                    devices = [
+                        "observer-pc"
+                        "observer-laptop"
+                    ];
                 };
 
-                folders = {
-                    documents = {
-                        path = "/home/observer/Documents";
-                        devices = [ "observer-pc" ]; # "observer-laptop"
-                    };
+                projects = {
+                    path = "/home/observer/projects";
 
-                    projects = {
-                        path = "/home/observer/projects";
-                        devices = [ "observer-pc" ]; # "observer-laptop"
-                    };
+                    devices = [
+                        "observer-pc"
+                        "observer-laptop"
+                    ];
                 };
             };
         };
