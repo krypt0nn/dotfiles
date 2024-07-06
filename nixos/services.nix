a4ac15dc92a5daa13637546ece844d7c97a3715c
Nikita Podvirnyi <krypt0nn@vk.com>
2024-07-06 19:29:29 +0200

    feat: updated `auto-cpufreq` service settings

--- nixos/services.nix
+++ nixos/services.nix
@@ -1,6 +1,20 @@
 { ... }: {
     # Enable automatic CPU frequency configuration
-    services.auto-cpufreq.enable = true;
+    services.auto-cpufreq = {
+        enable = true;
+
+        settings = {
+            battery = {
+                governor = "powersave";
+                turbo = "never";
+            };
+
+            charger = {
+                governor = "performance";
+                turbo = "auto";
+            };
+        };
+    };
 
     # Enable weekly SSD TRIM service (SSD optimization)
     services.fstrim.enable = true;
