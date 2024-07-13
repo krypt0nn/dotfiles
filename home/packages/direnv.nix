2f51737c4a3e3641d25df71333a1e47d3db077db
Nikita Podvirnyi <krypt0nn@vk.com>
2024-07-13 11:55:54 +0200

    feat: use local .cache folder to store direnvs

--- home/packages/direnv.nix
+++ home/packages/direnv.nix
@@ -1,7 +1,32 @@
-{ pkgs, ... }: {
+{ flakeConfig, ... }: {
     programs.direnv = {
         enable = true;
         nix-direnv.enable = true;
         enableBashIntegration = true;
+
+        stdlib = ''
+            : "''${XDG_CACHE_HOME:="''${HOME}/.cache"}"
+
+            declare -A direnv_layout_dirs
+
+            direnv_layout_dir() {
+                local hash path
+
+                echo "''${direnv_layout_dirs[$PWD]:=$(
+                    hash="$(sha1sum - <<< "$PWD" | head -c40)"
+                    path="''${PWD//[^a-zA-Z0-9]/-}"
+
+                    echo "''${XDG_CACHE_HOME}/direnv/layouts/''${hash}''${path}"
+                )}"
+            }
+        '';
+    };
+
+    home.persistence."/persistent/home/${flakeConfig.username}" = {
+        allowOther = false;
+
+        directories = [
+            ".cache/direnv"
+        ];
     };
 }
