ae0f9e4ace9b9e00fec7fd40d2f2ab5c32341efb
Nikita Podvirnyi <krypt0nn@vk.com>
2024-07-14 02:25:40 +0200

    feat: persist zed editor folders

--- home/apps/zed-editor.nix
+++ home/apps/zed-editor.nix
@@ -1,5 +1,14 @@
-{ pkgs-unstable, ... }: {
+{ flakeConfig, pkgs-unstable, ... }: {
     home.packages = with pkgs-unstable; [
         zed-editor
     ];
+
+    home.persistence."/persistent/home/${flakeConfig.username}" = {
+        allowOther = false;
+
+        directories = [
+            ".local/share/zed"
+            ".config/zed"
+        ];
+    };
 }
