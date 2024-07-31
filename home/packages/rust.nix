d544182992e11966be1f506cbf0ad37f8dfad003
Nikita Podvirnyi <krypt0nn@vk.com>
2024-07-31 08:55:15 +0200

    feat: changed rust package to use rustup

--- home/packages/rust.nix
+++ home/packages/rust.nix
@@ -1,15 +1,9 @@
 { flakeConfig, pkgs-unstable, ... }: {
     home.packages = with pkgs-unstable; [
-        cargo
-        clippy
-        rustc
-        rustfmt
+        rustup
     ];
 
-    home.sessionVariables = {
-        RUST_SRC_PATH = "${pkgs-unstable.rust.packages.stable.rustPlatform.rustLibSrc}";
-        RUST_BACKTRACE = 1;
-    };
+    home.sessionVariables.RUST_BACKTRACE = 1;
 
     home.persistence."/persistent/home/${flakeConfig.username}" = {
         allowOther = false;
