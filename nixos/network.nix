7fb05307916b7f85b18befe65b1a4039bf7fa2cf
Nikita Podvirnyi <krypt0nn@vk.com>
2024-09-21 22:04:17 +0200

    feat: weekly maintenance

--- nixos/network.nix
+++ nixos/network.nix
@@ -71,7 +71,7 @@
         # System-wide proxy
         proxy = lib.attrsets.optionalAttrs flakeConfig.network.global_proxy {
             default = "https://127.0.0.1:9050";
-            noProxy = "127.0.0.1,localhost,.localdomain";
+            noProxy = "127.0.0.1,::1,localhost,.localdomain";
         };
     };
 
@@ -111,17 +111,20 @@
             ClientTransportPlugin = "obfs4 exec ${pkgs.obfs4}/bin/lyrebird";
 
             Bridge = [
-                # Sep 18, 2024 | avg ping: 34 ms
-                "obfs4 57.129.55.85:44325 C3542E795BB2AEB4FD604914714BCB4374D1B603 cert=l2WgquhhPME6LgXRDaYD7V3UMnSGmiLoun2lnJ5ozRyKLMGBbYk0NtPxCptUZHv7ju7YOg iat-mode=0"
+                # Aug 22, 2024 | avg ping: 51 ms
+                # Aug 25, 2024 | avg ping: 96 ms
+                # Sep 01, 2024 | avg ping: 49 ms
+                # Sep 08, 2024 | avg ping: 48 ms
+                # Sep 15, 2024 | avg ping: 105 ms
+                # Sep 21, 2024 | avg ping: 34 ms
+                "obfs4 82.165.190.146:9443 4E2353218AB9DB222A7686A0E23A53358444641F cert=oWFrOozg/SFBcbk/09n6TIgzEe3TBmeiIlyPTMG/7iqwyUOPhSkpDRYQ16MaigMAxyL4HA iat-mode=0"
 
-                # Sep 15, 2024 | avg ping: 39 ms
-                "obfs4 37.59.25.181:29665 5755820AFDEB3149AC6F2FF6C558B3D335874C47 cert=jnjwkoZXFtB3UZnAmnTDQ+VyBteqrSBHn3xwZYvDHbJAsVldXxWp30TgezsPHWaekoQZag iat-mode=0"
-
-                # Sep 18, 2024 | avg ping: 40 ms
-                "obfs4 185.177.207.219:11219 598326DF0E32A1E1CCA87A10C9F614C104DD9DE9 cert=QJNjvqxkejHYUuMkM/cQmyV4Egj8S+f+7F4CPxcoWitFEsyP2dJXNQl8jU/M+5v9NeH4Hw iat-mode=1"
+                # Sep 21, 2024 | avg ping: 37 ms
+                "obfs4 91.134.105.105:30390 5988887624BD8271EF6B4071189757444DB92811 cert=lf6xZ2+2G6TFCO4Bh4HTt8a5ipG2062U8ToShRW0r7ij+BHSoN/E0NZlyr7lDa/1rIpQJg iat-mode=0"
 
                 # Sep 08, 2024 | avg ping: 38 ms
                 # Sep 15, 2024 | avg ping: 41 ms
+                # Sep 21, 2024 | avg ping: 37 ms
                 "obfs4 79.137.11.195:56288 CDAA4933BE540D25CF8F391E23F9A4861ACB5F4C cert=EicEZ20+CMP7iwmwgVujZOCtZHyIikzAmQOxrjAF2khU+IMm9iVN/tMDlYtoHSeCj11bVQ iat-mode=0"
 
                 # Aug 10, 2024 | avg ping: 39 ms
@@ -130,27 +133,45 @@
                 # Sep 01, 2024 | avg ping: 37 ms
                 # Sep 08, 2024 | avg ping: 38 ms
                 # Sep 15, 2024 | avg ping: 46 ms
+                # Sep 21, 2024 | avg ping: 38 ms
                 "obfs4 37.228.129.80:2056 B9A0ABC85F8FDECD3D73F8252A73C4BB22AAD3BD cert=I+5neLvuWJWl6NId2yvwo1sFqgthuIeYAtQY+q6I0mVz3ITLPo9AP35f8WJQsS8o2g5EOw iat-mode=0"
 
+                # Sep 18, 2024 | avg ping: 40 ms
+                # Sep 21, 2024 | avg ping: 39 ms
+                "obfs4 185.177.207.219:11219 598326DF0E32A1E1CCA87A10C9F614C104DD9DE9 cert=QJNjvqxkejHYUuMkM/cQmyV4Egj8S+f+7F4CPxcoWitFEsyP2dJXNQl8jU/M+5v9NeH4Hw iat-mode=1"
+
+                # Sep 18, 2024 | avg ping: 34 ms
+                # Sep 21, 2024 | avg ping: 42 ms
+                "obfs4 57.129.55.85:44325 C3542E795BB2AEB4FD604914714BCB4374D1B603 cert=l2WgquhhPME6LgXRDaYD7V3UMnSGmiLoun2lnJ5ozRyKLMGBbYk0NtPxCptUZHv7ju7YOg iat-mode=0"
+
                 # Sep 18, 2024 | avg ping: 52 ms
+                # Sep 21, 2024 | avg ping: 43 ms
                 "obfs4 57.129.34.114:60855 3C90AE902026BF557D225E0605E84B376B2095FD cert=wvHdL7i/Pemb87f7j6Bovxc4RqNd55iU1jR5pQ2btVwGZctOyEsKPbtj+zznllv4lq1EKQ iat-mode=0"
 
+                # Sep 21, 2024 | avg ping: 47 ms
+                "obfs4 51.91.210.144:61635 3C27E687FFF7989BFE05A9455F07007228CCD12A cert=fbV26rakbqZZzOJdVA6/dmV2HMkzot3yc1oqLf3cAKulP9NIgSTuMycS90jEr02xdvkoUw iat-mode=0"
+
                 # Sep 16, 2024 | avg ping: 64 ms
+                # Sep 21, 2024 | avg ping: 55 ms
                 "obfs4 51.77.111.182:49885 55414CB64AD283F7FBEB9643EBCB7E3A67ECB115 cert=+ZblXK36ALspn481jpywY0GgbPpbD6jF7ExE2+tQWYVkXfUP2P5f9jpornJWGh+jTSb/cg iat-mode=0"
+
+                # Sep 21, 2024 | avg ping: 60 ms
+                "obfs4 95.217.232.211:443 F32A7CD5589CCD2D700D790AB437DE8A6233372C cert=eqAb4zUDdQpKX9TFEuC/x82tiRjvlXkF/WnKxjjSuboPUB3URAwaXcn7m2GIzS8Kd2LDOQ iat-mode=0"
+
+                # Sep 15, 2024 | avg ping: 39 ms
+                # Sep 21, 2024 | avg ping: 61 ms
+                "obfs4 37.59.25.181:29665 5755820AFDEB3149AC6F2FF6C558B3D335874C47 cert=jnjwkoZXFtB3UZnAmnTDQ+VyBteqrSBHn3xwZYvDHbJAsVldXxWp30TgezsPHWaekoQZag iat-mode=0"
 
                 # Aug 18, 2024 | avg ping: 146 ms
                 # Aug 25, 2024 | avg ping: 61 ms
                 # Sep 01, 2024 | avg ping: 60 ms
                 # Sep 08, 2024 | avg ping: 62 ms
                 # Sep 15, 2024 | avg ping: 80 ms
+                # Sep 21, 2024 | avg ping: 61 ms
                 "obfs4 95.217.232.211:443 F32A7CD5589CCD2D700D790AB437DE8A6233372C cert=eqAb4zUDdQpKX9TFEuC/x82tiRjvlXkF/WnKxjjSuboPUB3URAwaXcn7m2GIzS8Kd2LDOQ iat-mode=0"
 
-                # Aug 22, 2024 | avg ping: 51 ms
-                # Aug 25, 2024 | avg ping: 96 ms
-                # Sep 01, 2024 | avg ping: 49 ms
-                # Sep 08, 2024 | avg ping: 48 ms
-                # Sep 15, 2024 | avg ping: 105 ms
-                "obfs4 82.165.190.146:9443 4E2353218AB9DB222A7686A0E23A53358444641F cert=oWFrOozg/SFBcbk/09n6TIgzEe3TBmeiIlyPTMG/7iqwyUOPhSkpDRYQ16MaigMAxyL4HA iat-mode=0"
+                # Sep 21, 2024 | avg ping: 62 ms
+                "obfs4 88.130.177.149:39002 7E1C25F432F5A79E0930612C307AA6ED11135A93 cert=OHp2D/Uk+b4za+7/8M4bBIemtFC3wr4fCmEvapfvv1FP+vwPemTahAAvpyM9wHfQwXysWw iat-mode=0"
             ];
 
             DNSPort = [
--- nixos/services.nix
+++ nixos/services.nix
@@ -1,27 +1,4 @@
 { ... }: {
-    # Enable automatic CPU frequency configuration
-    # Disabled now because I use GNOME Power Manager deamon
-    # which interfer with this service and prevents it normal work
-
-    # services.auto-cpufreq = {
-    #     enable = true;
-
-    #     settings = {
-    #         battery = {
-    #             governor = "powersave";
-    #             turbo = "never";
-    #         };
-
-    #         charger = {
-    #             governor = "performance";
-    #             turbo = "auto";
-    #         };
-    #     };
-    # };
-
-    # Control cooling depending on the system temperature
-    services.thermald.enable = true;
-
     # Enable weekly SSD TRIM service (SSD optimization)
     services.fstrim.enable = true;
 
@@ -31,12 +8,12 @@
     # Enable weekly firmware updates service
     services.fwupd.enable = true;
 
-    # Faster dbus implementation
-    services.dbus.implementation = "broker";
-
     # Load balancer
     services.irqbalance.enable = true;
 
     # Ban connections after failed login attempts
     services.fail2ban.enable = true;
+
+    # Faster dbus implementation
+    services.dbus.implementation = "broker";
 }
