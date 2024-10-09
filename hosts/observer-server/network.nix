483ad44e904edb04e84dc7be930b5c22de8206f7
Nikita Podvirnyi <krypt0nn@vk.com>
2024-10-09 17:34:42 +0200

    feat(server): temporary disable global proxy

--- hosts/observer-server/network.nix
+++ hosts/observer-server/network.nix
@@ -42,10 +42,10 @@
         ];
 
         # System-wide proxy
-        proxy = {
-            default = "https://observer-server:10050";
-            noProxy = "127.0.0.1,::1,localhost,.localdomain";
-        };
+        # proxy = {
+        #     default = "https://observer-server:10050";
+        #     noProxy = "127.0.0.1,::1,localhost,.localdomain";
+        # };
     };
 
     # SystemD DNS resolving service
