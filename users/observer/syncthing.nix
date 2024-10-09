7c7ae6f75303ba5cf9200738b9e718a7c0abf81a
Nikita Podvirnyi <krypt0nn@vk.com>
2024-10-09 20:28:15 +0200

    feat: added server syncthing

--- users/observer/syncthing.nix
+++ users/observer/syncthing.nix
@@ -14,6 +14,7 @@
             devices = {
                 "observer-pc"     = { id = "ANN4CJX-BOC36L6-72EYUZP-6B3J3LB-LYTF3PN-YL2JMYX-7U5BQMO-4V766A3"; };
                 "observer-laptop" = { id = "JWIJBIA-6ZQUP33-GB4OEC3-KQPW3CM-YFAZDDW-LQQNPWE-DI6WYXI-XVSVKQR"; };
+                "observer-server" = { id = "KBAD6RT-GHQCXW7-WYDFXF7-RKE5QOL-XSIUD6A-4E4D4G3-HLC75OZ-3ERYFQA"; };
             };
 
             folders = {
