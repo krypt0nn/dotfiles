f3882009941ab8b04fe87de7ee050ad634e7526d
Nikita Podvirnyi <krypt0nn@vk.com>
2024-12-23 11:29:46 +0200

    feat: added an-anime-team cachix repo

--- nixos/nix-config.nix
+++ nixos/nix-config.nix
@@ -15,12 +15,14 @@
                 "https://cache.nixos.org"
                 "https://nix-community.cachix.org"
                 "https://krypt0nn.cachix.org"
+                "https://an-anime-team.cachix.org"
             ];
 
             trusted-public-keys = [
                 "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
                 "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
                 "krypt0nn.cachix.org-1:ciP8xHjGQDDEjSW1LL9PO/fn8JRzm8zb57eUcFAblR8="
+                "an-anime-team.cachix.org-1:nr9QXfYG5tDXIImqxjSXd1b6ymLfGCvviuV8xRPIKPM="
             ];
         };
 
