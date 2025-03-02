61610e50b0b9c4d989146e9bb61fcdd14be36478
Nikita Podvirnyi <krypt0nn@vk.com>
2025-03-02 15:19:19 +0200

    feat: updated network settings

--- nixos/network.nix
+++ nixos/network.nix
@@ -21,7 +21,7 @@
         # Configure networking
         networkmanager = {
             enable = true;
-            dns = "systemd-resolved";
+            dns = "none";
         };
 
         # Setup hosts file
@@ -61,36 +61,49 @@
         ];
     };
 
-    # SystemD DNS resolving service
-    # Will be spawned under 127.0.0.53 address
-    services.resolved = {
-        # Enable DNS queries caching
-        enable = true;
-
-        fallbackDns = [
-            # Tailscale MagicDNS
-            "100.100.100.100"
-
-            # Local DNS
-            "127.0.0.1"
-            "::1"
-        ];
-    };
-
     # Tailscale
     services.tailscale.enable = true;
+
+    # Blocky
+    services.blocky = {
+        enable = true;
+
+        settings = {
+            ports.dns = 53;
+
+            upstreams.groups.default = [
+                "https://one.one.one.one/dns-query"
+            ];
+
+            bootstrapDns = {
+                upstream = "https://one.one.one.one/dns-query";
+
+                ips = [
+                    "1.1.1.1"
+                    "1.0.0.1"
+                    "2606:4700:4700::1111"
+                    "2606:4700:4700::1001"
+                ];
+            };
+
+            blocking = {
+                denylists = {
+                    ads = [
+                        "https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts"
+                    ];
+                };
+
+                clientGroupsBlock.default = [
+                    "ads"
+                ];
+            };
+        };
+    };
 
     # Tor
     services.tor = {
         enable = true;
-
-        client = {
-            # SOCKS5 proxy
-            enable = true;
-
-            # Local DNS
-            dns.enable = true;
-        };
+        client.enable = true;
 
         settings = {
             UseBridges = true;
@@ -99,15 +112,10 @@
             Bridge = [
                 "obfs4 83.226.191.248:2048 6FEC97F7437D5451211B42F35784FC2A436C2610 cert=3ubRj29BI4QtyMOVI/XiIE8tNTIypFqj5FtAtSnqMSdNu2ot43UL8PJ4U2c+asMV8YjZeA iat-mode=0"
                 "obfs4 150.43.248.236:443 E69FD1828D270D6F7DD94B1B2C5261CBF1A32583 cert=o3UL0xikRiafXXdnzXC3puRCDEaN7cRhwu+/0VnTcyVIgxFmg/Ds+LEGx2lKS/U7lYGLHQ iat-mode=0"
-                "obfs4 148.113.142.109:19611 04146098FCBC06D60B1399F6FDE0A1B7BC32BEDB cert=Pp8Xs7TuckTJovwGFlBfZ1tV+4fx6zzmyvuvf4M5EpcC7OOrAgyVVr8GgKOk2qP/Hi0YOQ iat-mode=0"
-                "obfs4 141.94.212.150:45891 5FEE9747D104A7ED8CBA5485C460A9E007C50242 cert=BW6HwBPzw1GJJUyyrxoSfQwMPuyX5+nxv1s61ix+Y0bwxVIXT9avlwT1X0gew1bMP3BjCw iat-mode=0"
-            ];
-
-            DNSPort = [
-                {
-                    addr = "0.0.0.0";
-                    port = 53;
-                }
+                "obfs4 37.120.238.182:26565 9C5515135BA35FDB3707CCF3311BC5FF9E2BA3ED cert=ktM7yOCEhCd4Ani9DAo4KGst4BiIp++5yyfCZDbPTz6nty1pn6vuXX14U5nhR+jpZNbSAQ iat-mode=0"
+                "obfs4 185.183.34.172:54452 4269C427EED688BBD47E925602A5C557619612AF cert=wUWLiqbQ6K2MnguODPJI5YyMWIBTJqcWJKytY6ZRlc9W5hrn6b/MpePplYkaJUk2cQHgNw iat-mode=0"
+                "obfs4 141.94.213.29:34975 3CCF6211A6115BA32224E939C179AC2F8269E186 cert=xFW3DRM458PUIuWgi74qRg8IG7JElyFJIYgy9+V+flBQuvfKKonuPJb373QooLLR+eIqMg iat-mode=0"
+                "obfs4 57.128.35.251:17275 3411026D454DF9F94BB263BCED2CFBECEBBAAF4C cert=nDA6sKSsjz28b1XZVZwZcx2dBbRSdYFZR5yvenhtFKQA7zYY6N2otTaa562gZViSNSW9Kg iat-mode=0"
             ];
 
             Address = "0.0.0.0";
