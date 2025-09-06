0ce192788bca22859bf4ae798fb9904dbb4acab5
Nikita Podvirnyi <krypt0nn@vk.com>
2025-09-06 23:35:01 +0200

    feat: remove warning popup from bottles

--- home/apps/bottles.nix
+++ home/apps/bottles.nix
@@ -1,5 +1,9 @@
 { flakeConfig, pkgs, ... }: {
-    home.packages = [ pkgs.bottles ];
+    home.packages = [
+        (pkgs.bottles.override {
+            removeWarningPopup = true;
+        })
+    ];
 
     home.persistence."/persistent/home/${flakeConfig.username}" = {
         allowOther = false;
