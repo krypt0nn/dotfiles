4a8c92578e1dc1b1499e6c4e8dbfe969bb57fa40
Nikita Podvirnyi <krypt0nn@vk.com>
2025-11-30 14:46:24 +0200

    fix(observer): fix the git config

--- home/users/observer.nix
+++ home/users/observer.nix
@@ -12,12 +12,10 @@
                 user = {
                     name = "Nikita Podvirnyi";
                     email = "krypt0nn@vk.com";
+                    signingkey = "signingkey";
                 };
 
-                signing = {
-                    signByDefault = true;
-                    key = "3B14311A878F6C8817482002859D416E5142AFF3";
-                };
+                commit.gpgsign = true;
 
                 safe.directory = "/system-flake";
             };
