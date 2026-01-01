4a123921e5b539f3bf1e23a4820870a3c4591fca
Nikita Podvirnyi <krypt0nn@vk.com>
2026-01-01 13:37:47 +0200

    feat(server): enable openssh

--- hosts/observer-server/services.nix
+++ hosts/observer-server/services.nix
@@ -14,4 +14,11 @@
             };
         };
     };
+
+    services.openssh = {
+        enable = true;
+        settings.PasswordAuthentication = false;
+    };
+
+    services.fail2ban.enable = true;
 }
