845fc8b64e96a47a4b3115e8e65e8411fd8402ca
Nikita Podvirnyi <krypt0nn@vk.com>
2026-01-18 09:45:06 +0200

    feat(apps/nixcord): add more plugins

--- home/apps/nixcord.nix
+++ home/apps/nixcord.nix
@@ -9,15 +9,21 @@
 
             plugins = {
                 anonymiseFileNames.enable = true;
+                alwaysTrust.enable = true;
+                betterGifPicker.enable = true;
                 forceOwnerCrown.enable = true;
                 showMeYourName.enable = true;
                 messageLatency.enable = true;
                 messageLogger.enable = true;
+                imageFilename.enable = true;
+                imageZoom.enable = true;
                 noUnblockToJump.enable = true;
                 silentTyping.enable = true;
                 whoReacted.enable = true;
                 validUser.enable = true;
                 summaries.enable = true;
+                showHiddenChannels.enable = true;
+                showHiddenThings.enable = true;
                 webScreenShareFixes.enable = true;
             };
         };
