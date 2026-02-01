d6d9783d3a8620329d8001be4bb0ed2d7823a2cc
Nikita Podvirnyi <krypt0nn@vk.com>
2026-02-01 18:29:27 +0200

    feat: remove `throne` pin

--- flake.nix
+++ flake.nix
@@ -49,35 +49,6 @@
             overlays = with (import ./overlays.nix); [
                 # Always use latest pre-compiled rust binaries.
                 rust-overlay.overlays.default
-
-                # Finally got throne update :xdd:
-                #
-                # https://github.com/NixOS/nixpkgs/pull/475843
-                # https://github.com/NixOS/nixpkgs/commit/e5d943983efb1dfdeba7c02d1f54344158d4e677
-                (pin {
-                    pkg = "throne";
-                    rev = "e5d943983efb1dfdeba7c02d1f54344158d4e677";
-                    hash = "05hvs4sj1y77qwxh0sq9sw1jjj86k51b48dzhpxchj7y32gx4wf4";
-                })
-
-                # Pin tun2proxy binary because it broke upstream
-                #
-                # The error is:
-                #   [ERROR tproxy_config::common] Failed to set up TProxy: routing loop detected
-                #   [ERROR tun2proxy_bin] main loop error: routing loop detected
-                #   Error: Custom { kind: Other, error: "routing loop detected" }
-                #
-                # Caused by tproxy-config update: https://github.com/tun2proxy/tproxy-config/commit/be337437195e5ede08b589308e8ddf4ccbacf431
-                # Which prevents tun2proxy to be used with locally served proxies
-                # (pin {
-                #     pkg = "tun2proxy";
-                #     rev = "b5cb7e25685cf4256f070edc3ea7791c8cd21613";
-                #     hash = "zwI4xXLLkCMmW3+OBTE7M0quE8G15ynJVBOrWs2uLe4=";
-                # })
-
-                # Overlay some apps to use local proxy.
-                # (proxy { pkg = "vesktop"; proxy = "socks5://127.0.0.1:11050"; })
-                # (proxy { pkg = "fragments"; proxy = "socks5://127.0.0.1:9050"; })
             ];
 
             config = {
