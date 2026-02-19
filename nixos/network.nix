2d2263d1d4a5fd5f3d968e6d1aeb88d929c12e1c
Nikita Podvirnyi <krypt0nn@vk.com>
2026-02-19 22:22:45 +0200

    feat: update network settings

--- nixos/network.nix
+++ nixos/network.nix
@@ -114,8 +114,13 @@
                 webtunnel [2001:db8:8719:f52e:5708:d05:377a:5494]:443 6476A667CC69D29B0AA42A421CA9A0D32698A505 url=https://wtb004.unshakled.net/pl51ucT70jwtW8tjhXL7waaI ver=0.0.3
                 webtunnel [2001:db8:603c:2434:873c:4d58:9fd0:91e0]:443 56626E4B996EB1C9F4C0B573888E8EE3C4E28472 url=https://kriptohomyaki.org/4USXwPrEoemgJOXPmgjxPOAkQa1UG7To ver=0.0.3
                 webtunnel [2001:db8:ce90:3593:272e:4975:a031:55b]:443 12382A2F3912AD1983A97C8709CBAE47ADB60BE3 url=https://miranda.today/LWwxIXDHCyyScn7oDauPMTmX ver=0.0.3
-                webtunnel [2001:db8:3c2f:9f49:8f90:55f7:3d6b:5ebb]:443 39ADE0EB1AC1438815CE81A28713445F2F07331E url=https://spitfire.express/7TcoipyyI0PWdH4zBwt7aBbt ver=0.0.3
                 webtunnel [2001:db8:e65a:afaf:7443:b5a5:f71d:ca4]:443 6FE31638D6084EDA6D94FADC99B0EDD109D6AE3A url=https://eu.g3wip.uk/7ePavP0vnTTelEJWeJ4NwpGE ver=0.0.1
+                webtunnel [2001:db8:87a6:51bd:ecd9:3d12:e0e3:1baa]:443 3683B1036F18DF4B560865C17AF85C373232A8D5 url=https://q.igrp.site/pg9PbqaxSvIbjtbVZMt9H7xF ver=0.0.3
+                webtunnel [2001:db8:d513:341f:d853:76fe:aaf1:dedc]:443 C94F0B257D1950B17BB2147021B0E07C5891007A url=https://r4fo.com/7z0hLnrTxvkPIHmuPH94Ju2J ver=0.0.3
+                webtunnel [2001:db8:9de4:c800:cc50:4c26:b192:b006]:443 1F5CA44230E96911836158A474E2AEB16EAEA57C url=https://ghosttown.dev/MTwam63OSHyUPQPcRRw8hx50 ver=0.0.2
+                webtunnel [2001:db8:e517:2fce:34d3:e541:53c5:9fe6]:443 ACBB486B9D60979A05E623D11CC8181A16A81E51 url=https://usa.uk.cloud-ip.cc/7gBqm1jbTOpU0jLV91IZHN0f ver=0.0.1
+                webtunnel [2001:db8:44:614b:42f5:f87c:7072:151]:443 F0C55D7B465C94F71A9A8F0479073BC02565DA36 url=https://quantentoast.de/CDgakmo0QO5Rvo2jRCtu4n9D ver=0.0.1
+                webtunnel [2001:db8:5f24:a703:dbde:bec8:d285:d600]:443 EFCEA1B038931F73BA155FE1E5945BCEBD62212C url=https://ttt.enerhome.site/ttt_TOR_Cjy8764jkUtopY ver=0.0.1
             ''));
 
             HardwareAccel = true;
