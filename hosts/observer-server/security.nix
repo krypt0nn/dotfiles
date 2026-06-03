9442157e9f7150a3fde269362ce65839f539084f
Nikita Podvirnyi <krypt0nn@vk.com>
2026-06-03 15:21:02 +0200

    feat(server): enforce security settings

--- hosts/observer-server/security.nix
+++ hosts/observer-server/security.nix
@@ -1,5 +1,5 @@
-{
-    boot.kernel.sysctl = {
+{ lib, ... }: {
+    boot.kernel.sysctl = lib.mkForce {
         # The Magic SysRq key is a key combo that allows users connected to the
         # system console of a Linux kernel to perform some low-level commands.
         # Disable it, since we don't need it, and is a potential security concern.
