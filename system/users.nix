99a89a8381d76a43a1064bfdd7ca1bcaca1796bd
Nikita Podvirnyi <krypt0nn@vk.com>
2026-06-03 14:59:09 +0200

    feat: set xdg variables

--- system/users.nix
+++ system/users.nix
@@ -45,6 +45,13 @@
         };
     };
 
+    environment.sessionVariables = {
+        XDG_CACHE_HOME  = "$HOME/.cache";
+        XDG_CONFIG_HOME = "$HOME/.config";
+        XDG_DATA_HOME   = "$HOME/.local/share";
+        XDG_STATE_HOME  = "$HOME/.local/state";
+    };
+
     environment.persistence."/persistent" = {
         hideMounts = true;
 
