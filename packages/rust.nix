2293ea213b9aad84965ecbddadb85bfbbcf0e5a8
Nikita Podvirnyi <krypt0nn@vk.com>
2026-06-22 13:32:36 +0200

    feat(packages/rust): add gcc and musl build target

--- packages/rust.nix
+++ packages/rust.nix
@@ -1,6 +1,13 @@
-{ username, pkgs-unstable, ... }: {
-    environment.systemPackages = [
+{ username, pkgs, pkgs-unstable, ... }: {
+    environment.systemPackages = with pkgs; [
+        gcc
+
         (pkgs-unstable.rust-bin.stable.latest.default.override {
+            targets = [
+                "x86_64-unknown-linux-gnu"
+                "x86_64-unknown-linux-musl"
+            ];
+
             extensions = [
                 "rust-src"
                 "rust-analyzer"
@@ -15,7 +22,6 @@
         hideMounts = true;
 
         users.${username}.directories = [
-            ".rustup"
             ".cargo"
         ];
     };
