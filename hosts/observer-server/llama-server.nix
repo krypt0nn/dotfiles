0d0e6cd461495994e23622080227060950bdef56
Nikita Podvirnyi <krypt0nn@dawn.wine>
2026-08-04 23:50:06 +0200

    feat: tweak models settings

--- hosts/observer-server/llama-server.nix
+++ hosts/observer-server/llama-server.nix
@@ -18,7 +18,7 @@ let
         [Qwen3.5-4B-UD-Q4_K_XL]
         alias = qwen3.5-4b
         spec-type = draft-mtp
-        spec-draft-n-max = 6
+        spec-draft-n-max = 2
         temperature = 0.7
         top-p = 0.8
         top-k = 20
@@ -26,37 +26,17 @@ let
         presence-penalty = 1.5
         repeat-penalty = 1.0
 
-        [Qwen3-1.7B-UD-Q4_K_XL]
-        alias = qwen3-1.7b
-        temperature = 0.6
-        top-p = 0.95
-        top-k = 20
-        min-p = 0.0
-
         [LFM2.5-8B-A1B-UD-IQ4_XS]
         alias = lfm2.5-8b-a1b
         temperature = 0.2
         top-k = 80
         repeat-penalty = 1.05
 
-        [LFM2.5-1.2B-Instruct-UD-Q6_K_XL]
-        alias = lfm2.5-1.2b
-        temperature = 0.05
-        top-p = 0.1
+        [LFM2.5-2.6B-Q8_0]
+        alias = lfm2.5-2.6b
+        temperature = 0.1
         top-k = 50
-        repeat-penalty = 1.05
-
-        [Bonsai-8B-Q1_0]
-        alias = bonsai-8b
-        ctx-size = 16384
-        cache-type-k = q4_0
-        cache-type-v = q4_0
-        spec-draft-type-k = q4_0
-        spec-draft-type-v = q4_0
-        temperature = 0.5
-        top-p = 0.9
-        top-k = 20
-        min-p = 0.0
+        repeat-penalty = 1.1
     '';
 in {
     systemd.services.llama-server = {
