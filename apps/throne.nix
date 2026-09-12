4a30a8a356dd2937823b50aa94b23a3cb95ce631
Nikita Podvirnyi <krypt0nn@dawn.wine>
2026-09-12 14:13:51 +0200

    feat(apps/throne): hack to use latest app version

--- apps/throne.nix
+++ apps/throne.nix
@@ -1,14 +1,30 @@
-{ username, ... }: {
-    programs.throne = {
-        enable = true;
-        tunMode.enable = true;
+{ pkgs, lib, ... }:
+let
+    # HACK: force-use latest throne before it gets merged to next stable nixos.
+    throneNixpkgs = fetchTarball {
+        url = "https://github.com/NixOS/nixpkgs/archive/62fae0242370ae97c3721003a32366a0cba3a96e.tar.gz";
+        sha256 = "0hpnspsik1ssc1l7a5vxglknycazrvr9j5yy1x0r0q6mfax2kinm";
     };
 
-    environment.persistence."/persistent" = {
-        hideMounts = true;
+    thronePkgs = import throneNixpkgs {
+        inherit (pkgs.stdenv.hostPlatform) system;
+        config = pkgs.config;
+        overlays = pkgs.overlays or [];
+    };
+in {
+    disabledModules = [ "programs/throne.nix" ];
+    imports = [ "${throneNixpkgs}/nixos/modules/programs/throne.nix" ];
 
-        users.${username}.directories = [
-            ".config/Throne"
-        ];
+    config = {
+        programs.throne = {
+            package = thronePkgs.throne;
+            enable = true;
+            tunMode.enable = true;
+        };
+    };
+
+    options.security.polkit.enablePkexecWrapper = lib.mkOption {
+        type = lib.types.bool;
+        default = true;
     };
 }
