2c3c63121093242b4eea49f57dd5a3d57e9d7c7a
Nikita Podvirnyi <krypt0nn@dawn.wine>
2026-09-14 23:14:18 +0200

    chore: remove unused config value

--- hosts/observer-server/chekist.nix
+++ hosts/observer-server/chekist.nix
@@ -72,7 +72,6 @@
                     };
                     enable_vision = true;
                     channel_context = 7;
-                    max_context = 65536;
                     max_loop_steps = 100;
                     http_fetch_max_size = "1mb";
                     stand_by_duration = "2m";
