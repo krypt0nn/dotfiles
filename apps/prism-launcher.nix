1911fca2d6ccf39761d3d1bedeb7e82aef32c6e3
Nikita Podvirnyi <krypt0nn@dawn.wine>
2026-09-25 22:49:55 +0200

    feat(apps/prism-launcher): add jdk8 (for mc 1.7.10)

--- apps/prism-launcher.nix
+++ apps/prism-launcher.nix
@@ -24,6 +24,8 @@ let
                 jdks = with pkgs; [
                     jdk25
                     jdk21
+                    jdk17
+                    jdk8
                 ];
             };
 
